20bf16c5e2b24b6acc2e1faa57bae2b837d62818
Aliagha Huseynli <aliagha.huseynli@gmail.com>
2022-04-20 20:20:45 +0300

    Add all

---
+++ RondomDice/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RondomDice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Dice1 = new System.Windows.Forms.PictureBox();
+            this.Dice2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dice1
+            // 
+            this.Dice1.Location = new System.Drawing.Point(56, 77);
+            this.Dice1.Name = "Dice1";
+            this.Dice1.Size = new System.Drawing.Size(121, 118);
+            this.Dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice1.TabIndex = 0;
+            this.Dice1.TabStop = false;
+            this.Dice1.Click += new System.EventHandler(this.Dice1_Click);
+            // 
+            // Dice2
+            // 
+            this.Dice2.Location = new System.Drawing.Point(234, 77);
+            this.Dice2.Name = "Dice2";
+            this.Dice2.Size = new System.Drawing.Size(121, 118);
+            this.Dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice2.TabIndex = 1;
+            this.Dice2.TabStop = false;
+            this.Dice2.Click += new System.EventHandler(this.Dice2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 302);
+            this.Controls.Add(this.Dice2);
+            this.Controls.Add(this.Dice1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DICE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Dice1;
+        private System.Windows.Forms.PictureBox Dice2;
+    }
+}
+
---
+++ RondomDice/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RondomDice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dice1.ImageLocation = System.IO.Path.GetFullPath("Photo/0.png");
+            Dice2.ImageLocation = System.IO.Path.GetFullPath("Photo/0.png");
+        }
+
+        public void ZarAt()
+        {
+            Random rnd = new Random();
+            int DiceValue1 = rnd.Next(1, 7);
+            int DiceValue2 = rnd.Next(1, 7);
+
+            Dice1.ImageLocation = System.IO.Path.GetFullPath("Photo/"+DiceValue1.ToString()+".png");
+            Dice2.ImageLocation = System.IO.Path.GetFullPath("Photo/"+DiceValue2.ToString()+".png");
+        }
+
+        private void Dice1_Click(object sender, EventArgs e)
+        {
+            ZarAt();
+            //Dice1.ImageLocation = System.IO.Path.GetFullPath("Photo/6.png");
+            //Dice2.ImageLocation = System.IO.Path.GetFullPath("Photo/6.png");
+        }
+
+        private void Dice2_Click(object sender, EventArgs e)
+        {
+            ZarAt();
+            //Dice1.ImageLocation = System.IO.Path.GetFullPath("Photo/6.png");
+            //Dice2.ImageLocation = System.IO.Path.GetFullPath("Photo/6.png");
+        }
+    }
+}
